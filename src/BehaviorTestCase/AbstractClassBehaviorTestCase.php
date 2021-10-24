b7f67aa8971b8b2f0e03249829d38db9bbaa3c9d
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-10-24 22:02:37 +0200

    Add ClassImplementTrait

--- src/BehaviorTestCase/AbstractClassBehaviorTestCase.php
+++ src/BehaviorTestCase/AbstractClassBehaviorTestCase.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PhpBehavior\BehaviorTestCase;
 
 use PHPUnit\Framework\TestCase;
-use Steevanb\VersionComparator\Comparator\PhpVersionComparator;
 
 abstract class AbstractClassBehaviorTestCase extends TestCase
 {
